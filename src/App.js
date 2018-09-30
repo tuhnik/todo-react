16b0e3a420a4d3de9127a64d114869a10f2e43fd
tuhnik <huntlarsen@gmail.com>
2018-09-30 10:50:04 +0300

    added css, remaining items counter

--- src/App.js
+++ src/App.js
@@ -27,18 +27,26 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
+      <div className="App container">
+      <h2 className = "title is-2">todo in react</h2>
          <form onSubmit={this.formSubmitted.bind(this)}>
-           <input onChange={this.inputChanged.bind(this)}type = "text" value = {this.state.newTodo}></input>
+           <input className = "input" onChange={this.inputChanged.bind(this)}type = "text" value = {this.state.newTodo}></input>
          </form>
-         <ul> {this.state.todos.map((el, i)=>{
-           return (<li key = {i}>
+         <div className = "todo-list">
+         {this.state.todos.map((el, i)=>{
+           return (<span className = "tag is-large todo-item" key = {i}>
+           <div className = "todo-left">
            <input onChange= {(evt) => this.toggleDone(evt, i)} type="checkbox"></input>
            <span style = {{"textDecoration": el.done ? "line-through": ""}}>{el.title}</span>
-           <button onClick = {()=>this.removeTodo(i)}>X</button>
-           </li>)
+          </div>
+           <button className = "delete" onClick = {()=>this.removeTodo(i)}></button>
+           </span>)
          })}
-         </ul>
+        </div>
+        {this.state.todos.length > 0 && <div>
+        <hr className = "todo-line"></hr>
+        <div className = "todo-items-remaining">{this.state.todos.filter(el=>!el.done).length} items remaining</div>
+        </div>}
       </div>
     );
   }
