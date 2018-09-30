d25ee0f1adda312b28fed3ad6b58b1af8baade05
tuhnik <huntlarsen@gmail.com>
2018-09-30 17:59:33 +0300

    breaking to pieces

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,21 +1,21 @@
 import React from 'react';
 const TopBar = (props) => {
 return(
-  <div>
-  {props.todos.length > 0 && <nav className = "todo-top-bar breadcrumb has-bullet-separator">
-<ul>
-<li className = {props.filter === "all"? "is-active": ""}> <a onClick={()=> {
-  props.changeState({filter: "all"})
-}}>All</a>     </li>
-<li className = {props.filter === "remaining"? "is-active": ""}><a onClick={()=> {
-  props.changeState({filter: "remaining"})    
-}}>Remaining</a></li>
- <li className = {props.filter === "done"? "is-active": ""}><a onClick={()=> {
-  props.changeState({filter: "done"})   
-}}>Done</a></li>
-</ul>
-</nav>}
-</div>)
+    <React.Fragment>
+        {props.todos.length > 0 && <nav className = "todo-top-bar breadcrumb has-bullet-separator">
+            <ul>
+                <li className = {props.filter === "all"? "is-active": ""}> <a onClick={()=> {
+                    props.changeState({filter: "all"})
+                    }}>All</a>     </li>
+                <li className = {props.filter === "remaining"? "is-active": ""}><a onClick={()=> {
+                    props.changeState({filter: "remaining"})    
+                    }}>Remaining</a></li>
+                <li className = {props.filter === "done"? "is-active": ""}><a onClick={()=> {
+                    props.changeState({filter: "done"})   
+                    }}>Done</a></li>
+            </ul>
+        </nav>}
+    </React.Fragment>)
 
 }
 
