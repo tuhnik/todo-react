4a39ab8c215d2b31e669c9b9ec8ee47f841934d3
tuhnik <huntlarsen@gmail.com>
2018-09-30 19:59:30 +0300

    breaking to pieces

--- src/TopBar.js
+++ src/TopBar.js
@@ -3,16 +3,11 @@ const TopBar = (props) => {
 return(
     <React.Fragment>
         {props.todos.length > 0 && <nav className = "todo-top-bar breadcrumb has-bullet-separator">
-            <ul>
-                <li className = {props.filter === "all"? "is-active": ""}> <a onClick={()=> {
-                    props.changeState({filter: "all"})
-                    }}>All</a></li>
-                <li className = {props.filter === "remaining"? "is-active": ""}><a onClick={()=> {
-                    props.changeState({filter: "remaining"})    
-                    }}>Remaining</a></li>
-                <li className = {props.filter === "done"? "is-active": ""}><a onClick={()=> {
-                    props.changeState({filter: "done"})   
-                    }}>Done</a></li>
+            <ul> {["all", "remaining", "done"].map(el=>{
+                return <li className = {props.filter === el? "is-active": ""}> <a onClick={()=> {
+                    props.changeState({filter: el})
+                    }}>{el}</a></li>
+            })}          
             </ul>
         </nav>}
     </React.Fragment>)
